9d1672403612236d7469362dc0896289e663bc52
patkaush <56513275+patkaush@users.noreply.github.com>
2024-04-18 21:40:04 -0500

    Initial Page

---
+++ src/data/user.js
@@ -0,0 +1,80 @@
+const INFO = {
+	main: {
+		title: "Reactfolio by truethari",
+		name: "Tharindu N.",
+		email: "mail@tharindu.dev",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		github: "https://github.com/patkaush",
+		linkedin: "https://www.linkedin.com/in/kaushik-pattipati/",
+		leetcode : "https://leetcode.com/patkaush/"
+	},
+
+	homepage: {
+		title: "Full-stack developer.",
+		description:
+			"I am a Full-stack developer with expertise in Java Frameworks. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
+	},
+
+	about: {
+		title: "I’m Tharindu N. I live in Sri Lanka, where I design the future.",
+		description:
+			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.",
+	},
+
+	articles: {
+		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+	},
+
+	projects: [
+		{
+			title: "Placement Information System",
+			description:
+				"Implemented a robust three-tier architecture web application focusing on recommending companies to users based on their interests and skill sets.", 
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Code",
+			link: "https://github.com/patkaush/PlacementGuide-Major-Project-",
+		},
+
+		{
+			title: "Public Service Complaint Register",
+			description:
+				"An android application which allows users to file municipal complaints.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Youtube Song Downloader and Editor",
+			description:
+				"A static web application authorizes the download of songs in Mp3 format from a specific website using web scraping.",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Mini-Wikipedia",
+			description:
+				"A dynamic web application that displays information fetched from a database.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Mini-Pacman",
+			description:
+				"A single-level 2-D game where a player has to collect the food (without being caught by the enemies) to win the game.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+	],
+};
+
+export default INFO;
